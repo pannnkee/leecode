84fc5c68a119d547b86272b5295f7d84b2e2379a
潘科 <522240909@qq.com>
2020-11-26 18:10:56 +0800

    practice 0

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ package main
 //输入：nums1 = [1,2,2,1], nums2 = [2,2]
 //输出：[2,2]
 func intersect(nums1 []int, nums2 []int) []int {
-
+	return nil
 }
 
 //14. 最长公共前缀
 //输入: ["flower","flow","flight"]
 //输出: "fl"
 func longestCommonPrefix(strs []string) string {
-
+	return ""
 }
 
 // 122. 买卖股票的最佳时机 II
@@ -22,7 +22,7 @@ func longestCommonPrefix(strs []string) string {
 //解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
 //随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
 func maxProfit(prices []int) int {
-
+	return 0
 }
 
 //189. 旋转数组
@@ -41,7 +41,7 @@ func rotate(nums []int, k int)  {
 //函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
 //你不需要考虑数组中超出新长度后面的元素。
 func removeElement(nums []int, val int) int {
-
+	return 0
 }
 
 //66. 加一
@@ -49,7 +49,7 @@ func removeElement(nums []int, val int) int {
 //输出：[1,2,4]
 //解释：输入数组表示数字 123。
 func plusOne(digits []int) []int {
-
+	return nil
 }
 
 //1. 两数之和
@@ -58,7 +58,7 @@ func plusOne(digits []int) []int {
 //因为 nums[0] + nums[1] = 2 + 7 = 9
 //所以返回 [0, 1]
 func twoSum(nums []int, target int) []int {
-
+	return nil
 }
 
 //15. 三数之和
@@ -70,7 +70,7 @@ func twoSum(nums []int, target int) []int {
 //  [-1, -1, 2]
 //]
 func threeSum(nums []int) [][]int {
-
+	return nil
 }
 
 //6. Z 字形变换
@@ -83,17 +83,17 @@ func threeSum(nums []int) [][]int {
 //E C   I H   N
 //T     S     G
 func convert(s string, numRows int) string {
-
+	return ""
 }
 
 func main() {
 	intersect([]int{1,2,2,1},[]int{2,2})
-	longestCommonPrefix([]string{"flower","flow","flight"})
-	maxProfit([]int{7,1,5,3,6,4})
-	rotate([]int{1,2,3,4,5,6,7}, 3)
-	removeElement([]int{3,2,2,3}, 3)
-	plusOne([]int{1,2,3})
-	twoSum([]int{2, 7, 11, 15}, 9)
-	threeSum([]int{-1, 0, 1, 2, -1, -4})
-	convert("LEETCODEISHIRING", 4)
+	//longestCommonPrefix([]string{"flower","flow","flight"})
+	//maxProfit([]int{7,1,5,3,6,4})
+	//rotate([]int{1,2,3,4,5,6,7}, 3)
+	//removeElement([]int{3,2,2,3}, 3)
+	//plusOne([]int{1,2,3})
+	//twoSum([]int{2, 7, 11, 15}, 9)
+	//threeSum([]int{-1, 0, 1, 2, -1, -4})
+	//convert("LEETCODEISHIRING", 4)
 }

097ff96241bf291a6f03969e01230cc082cdfaa0
潘科 <522240909@qq.com>
2020-12-24 16:08:49 +0800

    [.]

--- main.go
+++ main.go
@@ -96,17 +96,23 @@ func missingNumber(nums []int) int {
 
 
 // 链表
-//19. 删除链表的倒数第N个节点
-//给定一个链表: 1->2->3->4->5, 和 n = 2.
-//当删除了倒数第二个节点后，链表变为 1->2->3->5.
 
 // Definition for singly-linked list.
 
 type ListNode struct {
-  Val int
-  Next *ListNode
+	Val int
+	Next *ListNode
 }
+
+//19. 删除链表的倒数第N个节点
+//给定一个链表: 1->2->3->4->5, 和 n = 2.
+//当删除了倒数第二个节点后，链表变为 1->2->3->5.
+
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if head == nil {
+
+	}
+
 	return nil
 }
 
